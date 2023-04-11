a8c45e9b482df67da973319e96504dcee619d570
Andrey Kalenik <kalenik.andrey@inbox.ru>
2023-04-11 21:38:19 +0300

    add spiner to welocome page

--- src/pages/WelcomePage.tsx
+++ src/pages/WelcomePage.tsx
@@ -3,6 +3,7 @@ import { useEffect } from "react"
 import { useAppDispatch, useAppSelector } from '../hook'
 import { fetchProfile } from '../store/slices/athleteProfileSlice'
 import styled from "styled-components";
+import Spiner from "../components/Spiner";
 
 const WelcomePage:React.FC = () => {
 
@@ -12,24 +13,28 @@ const WelcomePage:React.FC = () => {
         dispatch(fetchProfile())
     },[dispatch])
 
-    const userProfile = useAppSelector((state) => state.athleteProfile.profile)
+    const {profile_medium, firstname, lastname, country, state} = useAppSelector((state) => state.athleteProfile.profile)
 
     return ( 
-        <PageTemplate>
-            <h2>Wellcome to React Snail App.</h2>
-            <p>This app shows the records of the greatest athlete of all time.</p>
-            <p>Athlete info</p>
-            <IMG
-                src = {userProfile.profile_medium}
-                width = "256px"
-                height= "256px"
-                alt = {`${userProfile.firstname} ${userProfile.lastname}`}
-            />
-            <p>{`${userProfile.firstname} ${userProfile.lastname}`}</p>
-            <p>{userProfile.country} {userProfile.state}</p>
-        </PageTemplate>
+        <>
+            { lastname === undefined ? <Spiner/> :
+                <PageTemplate>
+                    <h2>Wellcome to React Snail App.</h2>
+                    <p>This app shows the records of the greatest athlete of all time.</p>
+                    <p>Athlete info</p>
+                    <IMG
+                        src = {profile_medium}
+                        width = "256px"
+                        height= "256px"
+                        alt = {`${firstname} ${lastname}`}
+                    />
+                    <p>{firstname} {lastname}</p>
+                    <p>{country} {state}</p>
+                </PageTemplate>
+            }
+        </>
 
-     );
+     )
 }
 
 const IMG = styled.img`
